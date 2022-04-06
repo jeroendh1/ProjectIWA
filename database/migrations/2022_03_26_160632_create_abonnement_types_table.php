353a4534c2de9a6b641159789f3c8d185b066b11
StechWasTaken <41377140+StechWasTaken@users.noreply.github.com>
2022-04-06 15:18:42 +0200

    update

--- database/migrations/2022_03_26_160632_create_abonnement_types_table.php
+++ database/migrations/2022_03_26_160632_create_abonnement_types_table.php
@@ -14,13 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('abonnement_types', function (Blueprint $table) {
-<<<<<<< HEAD:database/migrations/2022_03_26_160632_create_abonnement_types_table.php
             $table->increments('id');
             $table->string('omschrijving');
-=======
-            $table->increments('abonnement_id');
-            $table->string('naam');
->>>>>>> db:database/migrations/2022_03_28_160612_create_abonnement_types_table.php
         });
     }
 
